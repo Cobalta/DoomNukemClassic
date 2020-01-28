40c30411d031db2cf48c4ae78cfb283b4a4ff528
Cobalta <liandrix31@gmail.com>
2020-01-28 16:32:59 +0100

    git tout pt

--- includes/doom.h
+++ includes/doom.h
@@ -5,28 +5,110 @@
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: ebourgeo <ebourgeo@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2019/11/28 15:18:19 by ebourgeo     #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/28 15:18:19 by ebourgeo    ###    #+. /#+    ###.fr     */
+/*   Created: 2020/01/23 16:26:07 by ebourgeo     #+#   ##    ##    #+#       */
+/*   Updated: 2020/01/23 16:26:07 by ebourgeo    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #ifndef DOOM_H
 # define DOOM_H
-
-# include <unistd.h>
-# include <fcntl.h>
-# include <math.h>
-# include "sdl2/SDL.h"
-# include "struct.h"
 # include "../libft/libft.h"
-# define PI 3.14159265359
-# define WIDTH 1280
-# define HEIGHT 960
+# include <pthread.h>
+# include <math.h>
+# include <../SDL/Headers/SDL.h>
+# include <../SDL/Headers/SDL_surface.h>
+# include <../SDL/Headers/SDL_video.h>
+# include <../SDL/Headers/SDL_render.h>
+# include <../SDL/Headers/SDL_pixels.h>
+# include <../SDL/Headers/SDL_events.h>
+#define WINH 1000
+#define WINL 1200
 
-void	setup(t_env *env);
-void	control(t_env *env, const Uint8 *keystates);
-void	line_tracer(t_seg *seg, t_env *env);
-void	render(t_env *env);
+typedef struct	s_dpos
+{
+	double			x;
+	double			y;
+}				t_dpos;
+
+typedef struct	s_qdpos
+{
+	t_dpos			a;
+	t_dpos			b;
+	t_dpos			c;
+	t_dpos			d;
+	int				min;
+	int				max;
+	int				go;
+
+}				t_qdpos;
+
+typedef struct	s_wall
+{
+	int			ypix;
+	int			xpix;
+	t_dpos		a;
+	t_dpos		b;
+	double		len;
+	int			portal;
+}				t_wall;
+
+typedef struct	s_sector
+{
+	int			top;
+	int			bot;
+	int			cwall;
+	t_wall		*wall;
+}				t_sector;
+
+typedef struct	s_map
+{
+	t_sector	*sect;
+	t_dpos		pos;
+	int			ctsector;
+	int			psct;
+	double		ang;
+	double		baseang;
+	int			basepsct;
+	t_dpos		basepos;
+}				t_map;
+
+typedef struct	s_event
+{
+	int		a;
+	int		w;
+	int		s;
+	int		d;
+	int		r;
+}				t_event;
+
+typedef struct	s_param
+{
+	SDL_Surface		*xxx;
+	SDL_Window		*win;
+	SDL_Renderer	*ren;
+	t_map			*map;
+	t_event			eve;
+	char			quit;
+	int				dx;
+	int				dy;
+	int				actual;
+	int				diff;
+}				t_param;
+
+int		checkcolls(t_map *map, t_sector *sect, double x, double y);
+void	doom(t_param *p);
+void	setup(t_param *p);
+void	error_func(int code);
+void	readmap(int fd, t_param *param);
+void	drawline(t_dpos *src, t_dpos *dst, t_param *param);
+void	drawtexedline(t_dpos *src, t_dpos *dst, t_param *p, t_wall *w);
+void	drawminimap(t_param *p, t_map *map);
+void	drawsector(t_param *p, int actual, int min, int max, int ans);
+void	event_manager(SDL_Event *e, t_param *p);
+void	getcoor(t_qdpos *coor, t_param *p, int i, int k);
+void	render(t_param *p, int i, int min, int max, int ans);
+void	wewillbuildawall(t_qdpos *coor, t_param *p, t_wall *w);
+void	wewillbuildaportal(t_qdpos *coor, t_param *p, int port, int i);
 
 #endif
